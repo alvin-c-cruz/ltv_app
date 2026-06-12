d2507e8288956f668c39266997d802d056a16b7c
alvin-c-cruz <alvinccruz12@gmail.com>
2026-06-12 20:43:27 +0800

    Change ltv_app server port from 5000 to 5001

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,6 @@ app = create_app()
 
 if __name__ == "__main__":
     host = socket.gethostbyname(socket.gethostname())
-    port = 5000
+    port = 5001
     print(f"Starting host @ http://{host}:{port}")
     app.run(host=host, port=port, debug=True)
